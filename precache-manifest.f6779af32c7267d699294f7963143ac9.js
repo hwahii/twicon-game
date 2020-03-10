13e251e7c8e5d914faeb840d5d499a74d6284e02
Huang-Yin Chung <in1102@livemail.tw>
2020-03-11 00:51:31 +0800

    Updates

---
+++ precache-manifest.f6779af32c7267d699294f7963143ac9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42db9b8e5c0068423d9421e49f8d1950",
+    "url": "/twicon-game/index.html"
+  },
+  {
+    "revision": "0620674877852dd22f47",
+    "url": "/twicon-game/static/css/main.358c0182.chunk.css"
+  },
+  {
+    "revision": "0fe733a722b9076ab4f4",
+    "url": "/twicon-game/static/js/2.8128a09e.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/twicon-game/static/js/2.8128a09e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0620674877852dd22f47",
+    "url": "/twicon-game/static/js/main.032af259.chunk.js"
+  },
+  {
+    "revision": "2817f025d4f328a13330",
+    "url": "/twicon-game/static/js/runtime-main.4d817ca3.js"
+  },
+  {
+    "revision": "96691523d99583eeb06c1a51906edac1",
+    "url": "/twicon-game/static/media/landing.96691523.png"
+  },
+  {
+    "revision": "61c76c88e3e624d63c8dbaa19b63eed3",
+    "url": "/twicon-game/static/media/twicon.61c76c88.woff"
+  },
+  {
+    "revision": "a657075b7392dbaf998c6c13121777bb",
+    "url": "/twicon-game/static/media/twicon.a657075b.woff2"
+  }
+]);
